91be02c6096e1bbd54e184e2414e1530f86508ba
chandinirayadurgam <44672808+chandinirayadurgam@users.noreply.github.com>
2025-02-14 12:38:34 -0500

    Create PaymentController.java

---
+++ PaymentController.java
@@ -0,0 +1,19 @@
+package com.payment.controller;
+
+import com.payment.model.Payment;
+import com.payment.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public Payment makePayment(@RequestBody Payment payment) {
+        return paymentService.processPayment(payment);
+    }
+}
